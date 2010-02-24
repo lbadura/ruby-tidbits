a2d60c38583315d17948ffe29c696a160b408619
Łukasz Badura <lukasz@niebo.net>
2010-02-24 21:31:24 +0100

    Added a simple web scraper to get currencies from NBP.

---
+++ nbpscraper.rb
@@ -0,0 +1,33 @@
+# A simple web scraper to get the recent currency data from NBP
+#
+require 'rubygems'
+require 'mechanize'
+
+class NBPScraper
+  # url to NBP's ratings of A class currencies
+  URL = 'http://www.nbp.pl/home.aspx?f=/kursy/kursya.html'  
+
+  def initialize(currency_code = 'EUR')
+    @currency_code = currency_code
+    @url = NBPScraper::URL
+    @page = Mechanize.new().get(@url)
+  end
+
+  def load_page
+    @agent.get(@url)
+  end
+
+  def price
+    regexp = Regexp.new(@currency_code)
+    @page.search('//tr[@valign="middle"]').each do |tr|
+      content = tr.search('td').each do |td|
+        if (regexp.match(td.content))
+          return td.next_element.content.sub(',','.').to_f
+        end
+      end
+    end
+  end
+end
+
+# usage
+# puts NBPScraper.new('EUR').price
